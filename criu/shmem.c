3bb033130e05a5ff33eecf514221a9557f046c65
Fyodor Bocharov <bocharovfedor@gmail.com>
2015-12-22 18:22:50 +0300

    shmem: implement used and dirty pages tracking
    To track anon shared vma pages usage we create a bitmap for it.
    Each bit in this bitmap corresponds to particular page in vma. Bit with
    value of 1 in this map states that page is used. Bit 0 states that the
    page wasn't ever used by any dumpee process.
    Page is considered used if it has at least one PME_PRESENT or PME_SWAP
    bit set in any dumpee process pagemap.
    Pages usage tracking allows us not to dump unused pages at all.
    
    To track anon shared vma pages dirtiness we create a bitmap for it.
    Each bit in this bitmap corresponds to particular page in vma. Bit with
    value of 1 in this map states that page is dirty. Bit 0 states that the
    page is clean.
    Page is considered dirty if it has at least one PME_SOFT_DIRTY bit set in
    any dumpee process pagemap.
    Dirty pages tracking allows not to dump used pages on incremental dumps
    if pages contents haven't changed.
    
    Signed-off-by: Fyodor Bocharov <fbocharov@yandex.ru>
    Signed-off-by: Eugene Batalov <eabatalov89@gmail.com>

--- criu/shmem.c
+++ criu/shmem.c
@@ -286,12 +286,63 @@ struct shmem_info_dump {
 	unsigned long	start;
 	unsigned long	end;
 	int		pid;
+	unsigned long	*pdirty_map;
+	unsigned long	*pused_map;
 
 	struct shmem_info_dump *next;
 };
 
 #define SHMEM_HASH_SIZE	32
 static struct shmem_info_dump *shmems_hash[SHMEM_HASH_SIZE];
+
+#define BLOCKS_CNT(size, block_size) (((size) + (block_size) - 1) / (block_size))
+
+static int expand_shmem(struct shmem_info_dump *si, unsigned long new_size)
+{
+	unsigned long nr_pages, nr_map_items, map_size,
+				nr_new_map_items, new_map_size;
+
+	nr_pages = BLOCKS_CNT(si->size, PAGE_SIZE);
+	nr_map_items = BLOCKS_CNT(nr_pages, sizeof(*si->pdirty_map) * 8);
+	map_size = nr_map_items * sizeof(*si->pdirty_map);
+
+	nr_pages = BLOCKS_CNT(new_size, PAGE_SIZE);
+	nr_new_map_items = BLOCKS_CNT(nr_pages, sizeof(*si->pdirty_map) * 8);
+	new_map_size = nr_new_map_items * sizeof(*si->pdirty_map);
+
+	BUG_ON(new_map_size < map_size);
+
+	si->pdirty_map = xrealloc(si->pdirty_map, new_map_size);
+	if (!si->pdirty_map)
+		return -1;
+	memzero(si->pdirty_map + nr_map_items, new_map_size - map_size);
+
+	si->pused_map = xrealloc(si->pused_map, new_map_size);
+	if (!si->pused_map)
+		return -1;
+	memzero(si->pused_map + nr_map_items, new_map_size - map_size);
+
+	si->size = new_size;
+	return 0;
+}
+
+static void update_shmem_pmaps(struct shmem_info_dump *si, u64 *map,
+		unsigned long off)
+{
+	unsigned long p, pcount, poff;
+
+	pcount = BLOCKS_CNT(si->size - off, PAGE_SIZE);
+	poff = BLOCKS_CNT(off, PAGE_SIZE);
+	for (p = 0; p < pcount; ++p) {
+		if (map[p] & PME_SOFT_DIRTY)
+			set_bit(p + poff, si->pdirty_map);
+		if (map[p] & PME_SWAP)
+			set_bit(p + poff, si->pused_map);
+		else if ((map[p] & PME_PRESENT) &&
+				((map[p] & PME_PFRAME_MASK) != kdat.zero_page_pfn))
+			set_bit(p + poff, si->pused_map);
+	}
+}
 
 static struct shmem_info_dump *shmem_find(struct shmem_info_dump **chain,
 		unsigned long shmid)
@@ -309,28 +360,34 @@ int add_shmem_area(pid_t pid, VmaEntry *vma, u64 *map)
 {
 	struct shmem_info_dump *si, **chain;
 	unsigned long size = vma->pgoff + (vma->end - vma->start);
-	(void)map;
 
 	chain = &shmems_hash[vma->shmid % SHMEM_HASH_SIZE];
 	si = shmem_find(chain, vma->shmid);
 	if (si) {
-		if (si->size < size)
-			si->size = size;
+		if (si->size < size) {
+			if (expand_shmem(si, size))
+				return -1;
+		}
+		update_shmem_pmaps(si, map, vma->pgoff);
+
 		return 0;
 	}
 
-	si = xmalloc(sizeof(*si));
+	si = xzalloc(sizeof(*si));
 	if (!si)
 		return -1;
 
 	si->next = *chain;
 	*chain = si;
 
-	si->size = size;
 	si->pid = pid;
 	si->start = vma->start;
 	si->end = vma->end;
 	si->shmid = vma->shmid;
+
+	if (expand_shmem(si, size))
+		return -1;
+	update_shmem_pmaps(si, map, vma->pgoff);
 
 	return 0;
 }
