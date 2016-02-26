965a012b4143db1f0e0fe230cfaeeddcf8c1b69c
Eugene Batalov <eabatalov89@gmail.com>
2016-02-26 11:39:32 +0300

    TODO
    Need to implement the following test:
    
    Нужно созадть дерево процессов, где:
    
    а) много процессов
    б) много разных шмемов между ними
    в) все они как-то рандомно трогают её
    
    а потом -- test/jenkins/criu-dedup.sh . Тест, который запускает
    это дерево и дампит его 8 раз с интервалом в 0.1 секунду с авто
    дедупом. Потом ресторит и просит всех проверить ничего ли не
    поменялось в процессе.

--- test/jenkins/criu-dedup.sh
+++ test/jenkins/criu-dedup.sh
@@ -2,10 +2,12 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run --all --report report --parallel 4 -f h --pre 2 --dedup -x maps04 -x maps007 || fail
+#./test/zdtm.py run --all --report report --parallel 4 -f h --pre 2 --dedup -x maps04 -x maps007 || fail
 
 # Additionally run these two as they touch a lot of
 # memory and it makes sense to additionally check it
 # with delays petween iterations
-./test/zdtm.py run -t zdtm/transition/maps007 --report report -f h --pre 8:.1 --dedup || fail
-./test/zdtm.py run -t zdtm/static/mem-touch   --report report -f h --pre 8:.1 --dedup || fail
+#./test/zdtm.py run -t zdtm/transition/maps007 --report report -f h --pre 8:.1 --dedup || fail
+#./test/zdtm.py run -t zdtm/static/mem-touch   --report report -f h --pre 8:.1 --dedup || fail
+./test/zdtm.py run -t zdtm/static/maps01   --report report -f h --pre 8:.1 --dedup || fail
+./test/zdtm.py run -t zdtm/static/maps06   --report report -f h --pre 8:.1 --dedup || fail
