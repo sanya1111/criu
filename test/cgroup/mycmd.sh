a6fd4057375b48fb14009d0cbd2f3df43473db92
sanya1 <okay123456@yandex.ru>
2016-03-02 18:41:58 +0300

    mycmd.sh changed

--- test/cgroup/mycmd.sh
+++ test/cgroup/mycmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./run_criu.sh clean
 cgexec -g cpu:mygroup ./run_criu.sh init &&
- ./run_criu.sh dump && 
- cgexec -g cpu:mygroup ./run_criu.sh restore
+./run_criu.sh dump && 
+./run_criu.sh restore
 
